ea6d011d31be37d2cc831092b30b41842a8352f9
766597324 <766597324@qq.com>
2016-12-12 20:24:02 +0800

    766597324<766597324@qq.com>框架修改

---
+++ src/main/java/cn/com/sckj/TomcatApplication.java
@@ -0,0 +1,42 @@
+package cn.com.sckj;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class TomcatApplication  extends SpringBootServletInitializer{
+
+	private static Log logger = LogFactory.getLog(TomcatApplication.class);
+	
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(TomcatApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TomcatApplication.class);
+	}
+	
+	@Bean
+	protected ServletContextListener listener() {
+		return new ServletContextListener() {
+			@Override
+			public void contextInitialized(ServletContextEvent sce) {
+				logger.info("^-^ ServletContext initialized");
+			}
+
+			@Override
+			public void contextDestroyed(ServletContextEvent sce) {
+				logger.info("^-^ ServletContext destroyed");
+			}
+		};
+	}
+	
+}
